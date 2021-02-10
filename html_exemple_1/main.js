eb87188f8d400114c1c31b56710987acdf63eb1a
Akamya <75986500+Akamya@users.noreply.github.com>
2021-02-10 04:45:17 -0800

    Update and rename script3.js to main.js

--- html_exemple_1/main.js
+++ html_exemple_1/main.js
@@ -1,6 +1,6 @@
 function afficher(param) {
 	var folder = "/img/"
-	var fileName = `${folder}HQ${param}.jpg`;
+	var fileName = `${folder}HQ${param}.jpg`; // or folder + "HQ" + param +".jpg, output will be the same
 	document.getElementById("img_big").src = fileName;
 	document.getElementById(`img_${param}`).classList.add("shadow");
 	console.log(fileName);
